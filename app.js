849f22b11b9f403c4d8a288aed8207f6a2f7c15f
Sam Ashurst <95435776+SamAshurst@users.noreply.github.com>
2022-02-18 14:25:42 +0000

    Refactoring removeCommentById to only query the db once

--- app.js
+++ app.js
@@ -12,17 +12,6 @@ app.use(express.json());
 
 app.use("/api", apiRouter);
 
-// app.get("/api/topics", getTopics);
-// app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
-// app.get("/api/articles/:article_id", getArticleById);
-// app.get("/api/articles", getArticles);
-//app.get("/api/users", getUsers);
-// app.get("/api", getEndpoints);
-
-// app.post("/api/articles/:article_id/comments", postCommentByArticleId);
-
-// app.patch("/api/articles/:article_id", patchArticleById);
-
 app.all("/*", invalidEndpoint);
 app.use(handlePsqlErrors);
 app.use(handleCustomErrors);
--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,10 +1,9 @@
 const { deleteCommentById } = require("../models/comments.models");
-const { checkCommentExists } = require("../models/utils");
 
 exports.removeCommentById = (req, res, next) => {
   const id = req.params.comment_id;
 
-  Promise.all([checkCommentExists(id), deleteCommentById(id)])
+  deleteCommentById(id)
     .then(() => {
       res.sendStatus(204);
     })
