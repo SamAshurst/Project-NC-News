8191c771667b4da411de30595f0aca89141e98cb
Sam Ashurst <95435776+SamAshurst@users.noreply.github.com>
2022-02-18 14:30:50 +0000

    Removing checkCommentExists utils function - no longer needed

--- models/utils.js
+++ models/utils.js
@@ -13,19 +13,6 @@ exports.checkArticleExists = (id) => {
     });
 };
 
-exports.checkCommentExists = (id) => {
-  return db
-    .query(`SELECT comment_id FROM comments WHERE comment_id = $1`, [id])
-    .then(({ rows: [comment] }) => {
-      if (!comment) {
-        return Promise.reject({
-          status: 404,
-          msg: "Sorry that id does not exist",
-        });
-      }
-    });
-};
-
 exports.checkTopicExists = (topic) => {
   if (!topic) {
     return Promise.resolve;
